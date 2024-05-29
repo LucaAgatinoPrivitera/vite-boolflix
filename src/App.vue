a5c33748c75f7882dce5c7d9eed45bdc7500feec
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-29 15:03:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+  <Header msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
