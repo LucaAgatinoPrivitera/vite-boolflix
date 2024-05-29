0d0205cbdfaf8c2a59bd29128313e8c871493dba
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-29 15:15:09 +0200

    Main come segnaposto fatto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   components: {
     Header,
+    Main
   }
 }
 </script>
 
 <template>
   <Header msg="Vite + Vue" />
+  <Main></Main>
 </template>
 
 <style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div id="segnaposto" class="w-100 bg-black">
+    
+  </div>
+</template>
+
+<style scoped>
+#segnaposto{
+    height: 90vh;
+}
+</style>
